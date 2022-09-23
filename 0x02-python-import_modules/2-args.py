17e518045cb706f34e8a0456c9769afce5863d99
Georgenduka <ndukageorgeveno@gmail.com>
2022-09-23 04:20:56 +0000

    print number and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len = len(sys.argv) - 1
+    if len == 0:
+        print("{} arguments.".format(len))
+    elif len == 1:
+        print("{} argument:".format(len))
+        print("{}: {}".format(1, sys.argv[1])
+    else:
+        print("{} arguments:".format(len))
+        for i in range(1, len + 1):
+            print("{}: {}".format(i, sys.argv[i])
