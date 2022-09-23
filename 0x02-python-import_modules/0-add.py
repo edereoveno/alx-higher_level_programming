a04739683cf93b73039b9a73f4a2e31022b7e28c
Georgenduka <ndukageorgeveno@gmail.com>
2022-09-23 03:17:07 +0000

    import module and pass values into function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-#import module and change its name
-import add_0 as add_
-#assign values to variables
-a = 1
-b = 2
-#pass variables as arguments in function and print
-print("{} + {} = {}".format(a,b,add_.add(a, b)))
+if __name__ == "__main__":
+    #import module and change its name
+    import add_0 as add_
+    #assign values to variables
+    a = 1
+    b = 2
+    #pass variables as arguments in function and print
+    print("{} + {} = {}".format(a,b,add_.add(a, b)))
