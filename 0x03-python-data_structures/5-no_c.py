c9b5360673ef78d75002fcc3c32169e7b90bec12
Georgenduka <ndukageorgeveno@gmail.com>
2022-09-26 17:44:34 +0000

    remove letters from strings

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    result = ''
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            continue
+        else:
+            result += i
+    return result
