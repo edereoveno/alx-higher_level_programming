ced5e618e920abc3635edc5e1f50da7313e3725a
Georgenduka <ndukageorgeveno@gmail.com>
2022-10-03 02:46:01 +0000

    print alphabet with alternating cases

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,9 +4,9 @@ i = 122
 while i >= 97:
     flag = 0
     if i % 2 == 1:
-        i = i -32
+        i = i - 32
         flag = 1
-    print("{:s}".format(chr(i)), end = "")
+    print("{:s}".format(chr(i)), end="")
     if flag == 1:
         i = i + 32
     i = i - 1
