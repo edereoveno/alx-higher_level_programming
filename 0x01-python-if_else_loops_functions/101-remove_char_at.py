30dc43c2414f4d9c9bd59c37fae43c960630ba67
georgenduka <ndukageorgeveno@gmail.com>
2023-01-02 07:11:14 +0100

    restarted sprint 2

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def remove_char_at(str, n):
-    copy = str[:]
-    if n >= 0:
-        copy = copy[:n] + copy[n + 1:]
-    return copy
--- 0x02-python-import_modules/0-add.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import add_0 as add_
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a , b, add_.add(a, b)))
