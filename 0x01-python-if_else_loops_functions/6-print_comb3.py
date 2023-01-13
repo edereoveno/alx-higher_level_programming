19f897cf008309477b5bfad76f26cdcfd174919a
georgenduka <ndukageorgeveno@gmail.com>
2023-01-13 16:01:16 +0100

    print different combinations of different digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@
 for num in range(0, 10):
     for number in range(1, 10):
         if number > num:
-            print("{}{}".format(num, number))
+            print("{}{}".format(num, number), end=(",\
+ " if int(str(num)+str(number)) < 89 else '\n'))
