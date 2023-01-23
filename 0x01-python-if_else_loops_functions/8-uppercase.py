b9236e93e2894fde566dbc8369ed8e2513221736
Georgenduka <ndukageorgeveno@gmail.com>
2023-01-23 14:44:28 +0000

    print string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-	output = ''
-	for character in str:
-		if ord(character) in range(97, 123):
-			output += chr(ord(character) - 32)
-		#elif ord(character) in range(65, 91):
-		else:
-			output += character
-	print("{}".format(output))
+    output = ''
+    for character in str:
+	if ord(character) in range(97, 123):
+            output += chr(ord(character) - 32)
+        else:
+            output += character
+    print("{}".format(output))
