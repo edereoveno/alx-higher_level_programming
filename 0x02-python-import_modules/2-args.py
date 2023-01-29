34d7cdd60ec03928f355b4d0423d12cc0b3ba7dc
georgenduka <ndukageorgeveno@gmail.com>
2023-01-29 15:34:16 +0100

    prints nts number and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if len(sys.argv) >= 3:
-    print("{} arguments".format(len(sys.argv) - 1))
-elif len(sys.argv) == 2:
-    print("{} argument".format(len(sys.argv) - 1))
-else:
-    print("{} arguments".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+    if len(sys.argv) >= 3:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
