01a1b2c4e057c6390e709dc49ef7e0069e327e00
georgenduka <ndukageorgeveno@gmail.com>
2023-02-04 16:01:20 +0100

    test addition of two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+    result = a + b
+    return result
