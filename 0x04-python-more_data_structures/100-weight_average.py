ea6fd8a4997e31ede376ac9b2d9c311fe523a967
georgenduka <ndkageorgeveno@gmail.com>
2023-02-12 20:04:32 +0100

    return average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!.usr/bin/python3
+
+def weight_average(my_list=[]):
+    numer = list(i[0] * i[1] for i in my_list)
+    denom = list(i[1] for i in my_list)
+    average = sum(numer) / sum(denom)
+    return average if my_list else 0
