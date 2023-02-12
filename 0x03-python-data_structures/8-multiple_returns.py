175de181e4ce188641b9b97b1a5967bea8dad1d5
georgenduka <ndkageorgeveno@gmail.com>
2023-02-12 01:13:55 +0100

    return tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
     new_tuple = ()
     if len(sentence) == 0:
         new_tuple = len(sentence), None
-    else:    
+    else:
         new_tuple = len(sentence), sentence[0]
     return new_tuple
