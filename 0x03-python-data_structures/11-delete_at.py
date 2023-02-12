013639d8d26ab37fdfaacabac0574b2ea38a71bd
georgenduka <ndkageorgeveno@gmail.com>
2023-02-12 02:11:05 +0100

    delete item at specific position in list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,12 +2,8 @@
 
 
 def delete_at(my_list=[], idx=0):
-    new_list = []
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     else:
-        for i in range(len(my_list) - 1):
-            if i == idx:
-                continue
-            new_list.append(my_list[i])
-        return new_list
+        del my_list[idx]
+    return my_list
