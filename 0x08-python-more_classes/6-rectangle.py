acf6456d5015fa6b0341e0218ac9f86ec474cbaa
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-14 14:05:13 +0100

    print out rectangle representation witg # chracter

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle():
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
-        
+
     def area(self):
         """returns area of rectangke"""
         return self.__width * self.__height
