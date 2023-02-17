6a44f88714e670be26d8d24efd0e7e9d40dd886c
georgenduka <ndkageorgeveno@gmail.com>
2023-02-17 14:18:10 +0100

    returns list of attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    Module that returns list of attributes and methods of an object
+"""
+
 
 def lookup(obj):
+    """Function returns list of attributes and methods of an object"""
     return dir(obj)
