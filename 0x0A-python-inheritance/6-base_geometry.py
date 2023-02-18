805548c64f3425488a9d488ee53830bb24a9c943
georgenduka <ndkageorgeveno@gmail.com>
2023-02-18 18:04:13 +0100

    empty class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module of a class with an unplemeted function"""
+
+
+class BaseGeometry():
+    """Class with empty function"""
+    def area(self):
+        raise Exception("area() is not implemented")
