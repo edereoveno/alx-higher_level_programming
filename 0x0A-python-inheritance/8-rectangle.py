eb0ef1c315deb47257401acbf64d68173bc67c1c
georgenduka <ndkageorgeveno@gmail.com>
2023-02-18 18:40:28 +0100

    Merge branch 'master' of https://github.com/Georgenduka/alx-higher_level_programming

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Module defines class Rectangle"""
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Inherits from BaseGeometry"""
     def __init__(self, width, height):
+        """Initialization of Rectangle class"""
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
