ff5cd99861505ee005a241547d70a9cfe0b84fac
georgenduka <ndkageorgeveno@gmail.com>
2023-02-19 16:42:30 +0100

    square class that inherits frim rectanglle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
     Square Object Module
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
