2005fa7b50fefec4f993945aa7bc81eb748959d4
georgenduka <ndkageorgeveno@gmail.com>
2023-02-19 16:42:30 +0100

    square class that inherits frim rectanglle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,10 +9,12 @@ class Square(Rectangle):
     """Square class"""
 
     def __init__(self, size):
+        """Initialization of square class"""
         super().integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """returns area of square"""
         return self.__size ** 2
 
     def __str__(self):
