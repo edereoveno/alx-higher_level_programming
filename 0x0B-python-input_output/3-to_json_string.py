6c072004aee0fb2e790c215bc61b04fac469dbda
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-20 12:15:46 +0100

    return JSON representation of object(string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module with function that return the JSON representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns JSON representation of object(string)"""
+    return str(json.dumps(my_obj))
