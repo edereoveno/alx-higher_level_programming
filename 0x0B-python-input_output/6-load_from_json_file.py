d75627e08628744761fd3e50a998950461726476
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-20 13:19:59 +0100

    return object represent by json string

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module with function that creates object from JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+        creates object from JSON file
+    """
+    with open(filename) as f:
+        return json.load(f)
