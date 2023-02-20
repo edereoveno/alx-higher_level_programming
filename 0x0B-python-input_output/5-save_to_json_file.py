e3660d059ef3fbd191602047e51020291dca643f
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-20 13:19:59 +0100

    return object represent by json string

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@ Module with function that write json representation of an object to text file
 import json
 
 
-def save_to_json(my_obj, filename):
-    with open(filename, "w", encoding='utf-8') as file:
-        file.write(json.dumps(file))
+def save_to_json_file(my_obj, filename):
+    """write an object to text ile using json representation"""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
