b4e66d5c9da366341caad350648d67fd8be08925
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-20 11:00:17 +0100

    write a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module contains function that writes string into text file"""
+
+
+def write_file(filename="", text=""):
+    with open('filename', 'w+', encoding="utf-8") as f:
+        write_data = f.write(text)
+        return write_data
