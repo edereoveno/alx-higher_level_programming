9aff3dbf4043cd28ba92c3c2f3f065caed07db45
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-20 12:57:49 +0100

    returns obkject represented by JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""module with function that returns from its JSON representation"""
+"""
+Module with function that returns from its JSON representation
+"""
+
 import json
 
 
 def to_json_string(my_str):
-    """returns an object represented by JSON string"""
+    """Function returns an object represented by JSON string"""
 
     return json.loads(my_str)
