5f0764839262b14cf8041f314f36f92e3e65f573
Georgenduka <ndukageorgeveno@gmail.com>
2023-02-20 11:13:16 +0100

    appends string to file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,5 @@
 def append_write(filename="", text=""):
     """appends string to file"""
     with open (filename, "w", encoding="utf-8") as f:
-        f.write(text)
+        append_data = f.write(text)
+        return append_data
