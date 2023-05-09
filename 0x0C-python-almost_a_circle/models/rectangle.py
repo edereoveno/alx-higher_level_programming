b9ed07aff801a72d8cffe6204fbf91336818be97
georgenduka <ndukageorgeveno@gmail.com>
2023-05-09 14:55:49 +0100

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,4 +96,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """return new str"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+        return("[Rectangle] ({}) {}/{} - {}/{}".format(
+                    self.id, self.__x, self.__y, self.__width, self.__height))
